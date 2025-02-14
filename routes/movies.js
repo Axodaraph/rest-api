d747c81383de12642f132dac5a06573656fc9b9f
Axodaraph <darielnotion@gmail.com>
2025-02-13 21:16:31 -0500

    añadiendo controladores

--- routes/movies.js
+++ routes/movies.js
@@ -1,77 +1,13 @@
-import { validateMovie, validatePartialMovie } from '../schemas/movies.js'
-
 import { Router } from 'express'
-import { MovieModel } from '../model/movie.js'
+import { MovieController } from '../controllers/movie.js'
 export const moviesRouter = Router()
 
-moviesRouter.get('/', async (req, res) => {
-  try {
-    const { genre } = req.query
-    const movies = await MovieModel.getAll({ genre })
-    res.json(movies)
-  } catch (error) {
-    res.status(500).json({ message: error.message })
-  }
-})
+moviesRouter.get('/', MovieController.getAll)
 
-moviesRouter.get('/:id', async (req, res) => {
-  try {
-    // path-to-regexp
-    const { id } = req.params
-    const movie = await MovieModel.getById({ id })
-    if (movie) return res.json(movie)
+moviesRouter.get('/:id', MovieController.getById)
 
-    res.status(404).json({ message: 'Movie not found' })
-  } catch (error) {
-    res.status(500).json({ message: error.message })
-  }
-})
+moviesRouter.post('/', MovieController.create)
 
-moviesRouter.post('/', async (req, res) => {
-  try {
-    const result = validateMovie(req.body)
+moviesRouter.patch('/:id', MovieController.update)
 
-    if (result.error) {
-      return res.status(400).json({ error: JSON.parse(result.error.message) })
-    }
-
-    // en base de datos
-    const newMovie = await MovieModel.create({ input: result.data })
-    res.status(201).json(newMovie) // actualizar la cache del cliente
-  } catch (error) {
-    res.status(500).json({ message: error.message })
-  }
-})
-
-moviesRouter.patch('/:id', async (req, res) => {
-  try {
-    const result = validatePartialMovie(req.body)
-
-    if (!result.success) {
-      return res.status(400).json({ error: JSON.parse(result.error.message) })
-    }
-
-    const { id } = req.params
-    const updateMovie = await MovieModel.update({ id, input: result.data })
-
-    return res.json(updateMovie)
-  } catch (error) {
-    res.status(500).json({ message: error.message })
-  }
-})
-
-moviesRouter.delete('/:id', async (req, res) => {
-  try {
-    const { id } = req.params
-
-    const result = MovieModel.delete({ id })
-
-    if (result === false) {
-      return res.status(404).json({ message: 'Movie not found' })
-    }
-
-    return res.json({ message: 'Movie deleted' })
-  } catch (error) {
-    res.status(500).json({ message: error.message })
-  }
-})
+moviesRouter.delete('/:id', MovieController.delete)
